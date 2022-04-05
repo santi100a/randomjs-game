bc1d0f9cdccaf6c3fccdd2b05e5defee336dd302
Santi <95766532+santi100a@users.noreply.github.com>
2022-04-05 15:56:18 -0500

    Modified README.md and index files

--- README.md
+++ README.md
@@ -5,4 +5,13 @@ to guess the number. If it is wrong, then it gives you an error and
 encourages you to try again; if you manage to guess the right number, 
 then it cheers you on and asks you if you want to play again. If you 
 answer 'y' then it continues; if you answer 'n', it exits; if you 
-specify nothing or something invalid, then if defaults to 'y'. 
+specify nothing or something invalid, then it defaults to 'y'. 
+
+To run this game, you'll have to:
+<ul>
+    <li>Clone this repo by running <code>git clone https://github.com/santi100a/random-number-game-javascript.git</code>. </li>
+    <li>Erase the <code>node_modules</code>folder. </li>
+    <li>Run <code>npm install</code>. </li>
+    <li>Run <code>npm run test</code> or <code>node .</code>. </li>
+    <li>Enjoy the game! </li>
+</ul>
--- index.js
+++ index.js
@@ -12,20 +12,35 @@ if (!playerName || !playerName.length)
 guess = random(maximum);
 while (true) {
     answer = Number(input(`What\'s your guess (between 0 and ${maximum})? `));
-    if (guess === answer) {
+    if (guess === answer && guess <= maximum) {
+        console.clear();
         console.log(Chalk.green(`That\'s correct! Well done, ${playerName}! `));
         continueToPlay = input(`Do you want to play again (${Chalk.green('y')}/${Chalk.red('n')}), default is ${Chalk.gray('y')}? `);
         continueToPlay = continueToPlay[0];
         if (continueToPlay !== 'y' && continueToPlay !== 'n')
             continueToPlay = 'y';
         if (continueToPlay === 'y' || continueToPlay === 'Y') {
+            console.clear();
             guess = random(maximum);
             continue;
         }
         else if (continueToPlay === 'n' || continueToPlay === 'N')
             process.exit(0);
     }
-    else {
+    else if (guess > maximum) {
+        console.log(Chalk.cyan(`
+            You specified a number greater than ${maximum}. 
+            Don't do that. Try again.
+            `));
+    }
+    else if (guess < 0) {
+        console.log(Chalk.magenta(`
+            You specified a number smaller than 0. 
+            Don't do that. Try again.
+            `));
+    }
+    else if (guess <= maximum) {
+        console.clear();
         console.log(Chalk.red(`That\'s wrong! Try again, ${playerName}! `));
         continue;
     }
