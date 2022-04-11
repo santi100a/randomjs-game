2402c63438f8e23c12da42f962104db64ac510df
Santi <95766532+santi100a@users.noreply.github.com>
2022-04-10 19:15:22 -0500

    Modified documentation file for custom read input library

--- lib/readInput.d.ts
+++ lib/readInput.d.ts
@@ -2,7 +2,7 @@
  * A function that reads input from the standard input in Node.js.
  * 
  * @param message The message to display when the user is prompted.
- * By default it is an empty string. 
+ * By default, it is an empty string. 
  * @returns The input received from the user.  
  */
 
